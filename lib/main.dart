204586141a76696e2aea14208f2b568bbbf677f7
Ashik03074 <ashik03074@gmail.com>
2021-06-10 03:28:12 +0600

    This time data of jsonPlaceholderPosts are fetched by PODO pattern

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kala_app/models/parsing_json/json_parsing.dart';
+import 'package:kala_app/models/parsing_json/json_parsing_map.dart';
 
 void main() => runApp(MaterialApp(
-      home: JsonParsingSimple(),
+      //home: JsonParsingSimple(),
+      home: JsonParsingMap(),
     ));
