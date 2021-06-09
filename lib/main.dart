e2e55e6ccc3feaa408916eafc5323671371a77c1
Ashik03074 <ashik03074@gmail.com>
2021-06-09 17:28:56 +0600

    Parsing Jsoonplaceholder.post in flutter 1.22.4 version

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:kala_app/models/parsing_json/json_parsing.dart';
+
+void main() => runApp(MaterialApp(
+      home: JsonParsingSimple(),
+    ));
